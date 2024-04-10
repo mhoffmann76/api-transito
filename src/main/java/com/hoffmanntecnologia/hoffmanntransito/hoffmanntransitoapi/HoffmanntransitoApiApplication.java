8256934ca023acc91823267048957ba657f4c384
Marcelo Hoffmann <hoffmannmarcelo@hotmail.com>
2024-04-10 14:13:29 -0300

    initial

---
+++ src/main/java/com/hoffmanntecnologia/hoffmanntransito/hoffmanntransitoapi/HoffmanntransitoApiApplication.java
@@ -0,0 +1,13 @@
+package com.hoffmanntecnologia.hoffmanntransito.hoffmanntransitoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoffmanntransitoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoffmanntransitoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hoffmanntecnologia/hoffmanntransito/hoffmanntransitoapi/HoffmanntransitoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoffmanntecnologia.hoffmanntransito.hoffmanntransitoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoffmanntransitoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
